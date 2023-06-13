40144f350b0c7d445cdf8effed418607c803da94
Andres Romero <andresfelipe328@hotmail.com>
2023-06-13 16:07:36 -0700

    initial commit

--- components/BasicAnimationLayout.tsx
+++ components/BasicAnimationLayout.tsx
@@ -5,35 +5,35 @@ import { gsap } from "gsap";
 
 const BasicAnimationLayout = ({ children }: { children: React.ReactNode }) => {
   const root = useRef<HTMLDivElement>(null);
-  // useLayoutEffect(() => {
-  //   let ctx = gsap.context(() => {
-  //     // gsap.from(root.current!.childNodes, {
-  //     //   opacity: 0,
-  //     //   x: -10,
-  //     //   delay: 0.5,
-  //     //   duration: 2,
-  //     //   stagger: 0.2,
-  //     //   ease: "elastic.out(2, 0.5)",
-  //     // });
-  //     gsap.fromTo(
-  //       root.current!.childNodes,
-  //       {
-  //         opacity: 0,
-  //         x: -10,
-  //       },
-  //       {
-  //         delay: 0.5,
-  //         duration: 2,
-  //         opacity: 1,
-  //         x: 0,
-  //         stagger: 0.2,
-  //         ease: "elastic.out(2, 0.5)",
-  //       }
-  //     );
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      // gsap.from(root.current!.childNodes, {
+      //   opacity: 0,
+      //   x: -10,
+      //   delay: 0.5,
+      //   duration: 2,
+      //   stagger: 0.2,
+      //   ease: "elastic.out(2, 0.5)",
+      // });
+      gsap.fromTo(
+        root.current!.childNodes,
+        {
+          opacity: 0,
+          x: -10,
+        },
+        {
+          delay: 0.5,
+          duration: 2,
+          opacity: 1,
+          x: 0,
+          stagger: 0.2,
+          ease: "elastic.out(2, 0.5)",
+        }
+      );
 
-  //     return () => ctx.revert();
-  //   }, root);
-  // }, []);
+      return () => ctx.revert();
+    }, root);
+  }, []);
 
   return (
     <main className={`flex min-h-screen gap-2 bg-main-bg p-2`} ref={root}>
