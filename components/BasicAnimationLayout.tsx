69acd72131a1eacd3493af7a38669e9fc880ba8c
Andres Romero <andresfelipe328@hotmail.com>
2023-06-14 13:11:51 -0700

    initial commit

--- components/BasicAnimationLayout.tsx
+++ components/BasicAnimationLayout.tsx
@@ -6,43 +6,30 @@ import { gsap } from "gsap";
 const BasicAnimationLayout = ({ children }: { children: React.ReactNode }) => {
   const root = useRef<HTMLDivElement>(null);
   useLayoutEffect(() => {
+    document.body.classList.remove("noJS");
     let ctx = gsap.context(() => {
-      // gsap.to(root.current!.childNodes, {
-      //   opacity: 1,
-      //   x: 0,
-      //   delay: 0.5,
-      //   duration: 2,
-      //   stagger: 0.2,
-      //   ease: "elastic.out(2, 0.5)",
-      // });
-
-      gsap.fromTo(
-        root.current!.childNodes,
-        {
-          opacity: 0,
-          x: -10,
-        },
-        {
-          delay: 0.5,
-          duration: 2,
-          opacity: 1,
-          x: 0,
-          stagger: 0.2,
-          ease: "elastic.out(2, 0.5)",
-        }
-      );
-      // gsap.from(root.current, { opacity: 1, duration: 0.5, autoAlpha: 0 });
+      gsap.to(root.current, {
+        duration: 1,
+        x: 0,
+        opacity: 1,
+        stagger: 0.2,
+        delay: 0.4,
+        ease: "elastic.out(1, 0.75)",
+      });
 
       // gsap.fromTo(
       //   root.current!.childNodes,
-      //   { opacity: 0, y: 5 },
       //   {
-      //     duration: 0.25,
-      //     y: 0,
+      //     opacity: 0,
+      //     x: -10,
+      //   },
+      //   {
+      //     delay: 0.5,
+      //     duration: 2,
       //     opacity: 1,
+      //     x: 0,
       //     stagger: 0.2,
-      //     delay: 0.05,
-      //     ease: "power1.out",
+      //     ease: "elastic.out(2, 0.5)",
       //   }
       // );
 
@@ -51,7 +38,11 @@ const BasicAnimationLayout = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   return (
-    <main className={`flex gap-2 h-screen bg-main-bg p-2`} ref={root}>
+    <main
+      className={`flex gap-2 h-screen bg-main-bg p-2 -translate-x-5 overflow-hidden root`}
+      ref={root}
+      style={{ opacity: 0 }}
+    >
       {children}
     </main>
   );
