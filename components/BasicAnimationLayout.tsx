da554bad80469587a2837a8e4d90994a87d5dcfe
Andres Romero <andresfelipe328@hotmail.com>
2023-06-14 08:22:40 -0700

    initial commit

--- components/BasicAnimationLayout.tsx
+++ components/BasicAnimationLayout.tsx
@@ -7,39 +7,39 @@ const BasicAnimationLayout = ({ children }: { children: React.ReactNode }) => {
   const root = useRef<HTMLDivElement>(null);
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      // gsap.from(root.current!.childNodes, {
-      //   opacity: 0,
-      //   x: -10,
+      // gsap.to(root.current!.childNodes, {
+      //   opacity: 1,
+      //   x: 0,
       //   delay: 0.5,
       //   duration: 2,
       //   stagger: 0.2,
       //   ease: "elastic.out(2, 0.5)",
       // });
-      gsap.fromTo(
-        root.current!.childNodes,
-        {
-          opacity: 0,
-          x: -10,
-        },
-        {
-          delay: 0.5,
-          duration: 2,
-          opacity: 1,
-          x: 0,
-          stagger: 0.2,
-          ease: "elastic.out(2, 0.5)",
-        }
-      );
+
+      window.addEventListener("load", function (e: any) {
+        gsap.fromTo(
+          root.current!.childNodes,
+          {
+            opacity: 0,
+            x: -10,
+          },
+          {
+            delay: 0.5,
+            duration: 2,
+            opacity: 1,
+            x: 0,
+            stagger: 0.2,
+            ease: "elastic.out(2, 0.5)",
+          }
+        );
+      });
 
       return () => ctx.revert();
     }, root);
   }, []);
 
   return (
-    <main
-      className={`flex min-h-screen gap-2 bg-main-bg p-2 fallback`}
-      ref={root}
-    >
+    <main className={`flex min-h-screen gap-2 bg-main-bg p-2`} ref={root}>
       {children}
     </main>
   );
